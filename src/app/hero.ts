8fd2dce7bb75dae184c5cf590d505d71e75b0d6a
Poorna Chandra Tejas.J <poornachandratejas.j@MacBook-Air.local>
2018-12-15 17:21:49 +0530

    dashboard

---
+++ src/app/hero.ts
@@ -0,0 +1,14 @@
+export class Hero {
+  id: number;
+  name: string;
+  email: any;
+  phone: any;
+  altno: any;
+  street: string;
+  city: string;
+  address: any;
+  pincode: any;
+  gender: string;
+  dob: any;
+  heroimg: any;
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Niranjan', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Bangalore', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/catty.jpg'},
+  { id: 2, name: 'Spoorti B R',email: 'spoorti@gmail.com', phone: '8553423216', altno: '9725682682', street:'kumarswamy layout', city: 'Mysore', address: 'kumarswamy layout', pincode:'560078', gender:'female', dob:'03-12-1995',heroimg: '../assets/Spoorthy.jpg'},
+  { id: 3, name: 'Pradeep', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Thumkur', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/pradeep.jpg' },
+  { id: 4, name: 'Bhanu', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Shivmoga', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/bhanu.jpg' },
+  { id: 5, name: 'Pruthvi', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Agra', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/pruthvi.jpg' },
+  { id: 6, name: 'Balaji', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Mangaluru', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/balaji.jpg' },
+  { id: 7, name: 'Vikas', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Udupi', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/vikas.jpg' },
+  { id: 8, name: 'Varun', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Ramanagara', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/varun.jpg'},
+  { id: 9, name: 'Kiran', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Davanagere', address: 'kumarswamy layout', pincode:'560078', gender:'male', dob:'03-12-1995',heroimg: '../assets/kiran.jpg' },
+  { id: 10, name: 'Bindu', email: 'nice@gmail.com', phone: '9602132323', altno: '9725682682', street:'kumarswamy layout', city: 'Behar', address: 'kumarswamy layout', pincode:'560078', gender:'female', dob:'03-12-1995',heroimg: '../assets/bindu.jpg' }
+];
